45f371a992ac94099568a087d82b1cbeb5db6d37
pablop11 <palaciocobopablo@gmail.com>
2025-11-07 18:17:11 +0100

    sript gold y silver

---
+++ BDA/UNIDAD1/act1/src/gold_ingest.py
@@ -0,0 +1,23 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import to_date, col, sum 
+
+spark = SparkSession.builder.appName("gold").getOrCreate()
+
+ventas = spark.read.parquet("silver/ventas")
+
+ventas_por_dia = (ventas.groupBy("fecha").agg(
+sum("importe").alias("ventas_totales"),
+sum("unidades").alias("unidades_totales")
+).orderBy("fecha"))
+
+ventas_por_dia.write.mode("overwrite").parquet("gold/ventas_por_dia")
+
+top5 = ventas.groupBy("id_producto").agg(
+sum("unidades").alias("unidades_totales"),
+sum("importe").alias("importe_total")
+).orderBy(col("unidades_totales").desc()).limit(5)
+
+top5.write.mode("overwrite").parquet("gold/top5_productos")
+
+print("Gold listo")
+spark.stop()
--- BDA/UNIDAD1/act1/src/silver_ingest.py
+++ BDA/UNIDAD1/act1/src/silver_ingest.py
@@ -6,17 +6,41 @@ spark = SparkSession.builder.appName("silver").getOrCreate()
 
 ventas = spark.read.parquet("bronze/ventas")
 clientes = spark.read.parquet("bronze/clientes")
+facturas = spark.read.parquet("bronze/facturas_meta")
 
 ventas = (ventas
 .withColumn("id_venta", col("id_venta").cast(IntegerType()))
 .withColumn("fecha", to_date(col("fecha")))
 .withColumn("id_cliente", col("id_cliente").cast(IntegerType()))
 .withColumn("unidades", col("unidades").cast(IntegerType()))
-.withColumn("importe", col("importe").cast(DecimalType(12, 2)))
+.withColumn("importe", col("importe").cast(DecimalType(10, 2)))
 .dropDuplicates(["id_venta"])
 )
 
-ventas.write.mode("overwrite").parquet("silver/ventas")
+ids = clientes.select(col("id_cliente").cast(IntegerType()).alias("id_cliente"))
+
+ventas_ok = (ventas
+.join(ids, "id_cliente", "inner")
+.where((col("unidades") > 0) & (col("importe") >= 0))
+)
+
+clientes = (clientes
+.withColumn("id_cliente", col("id_cliente").cast(IntegerType()))
+.withColumn("fecha_alta", to_date(col("fecha_alta")))
+.dropDuplicates(["id_cliente"])
+)
+
+facturas = (facturas
+.withColumn("id_cliente", col("id_cliente").cast(IntegerType()))
+.withColumn("importe_total", col("importe_total").cast(DecimalType()))
+.filter(col("importe_total") > 0)
+.withColumn("fecha", to_date(col("fecha")))
+.dropDuplicates(["id_factura"])
+)
+
+ventas_ok.write.mode("overwrite").parquet("silver/ventas")
+clientes.write.mode("overwrite").parquet("silver/clientes")
+facturas.write.mode("overwrite").parquet("silver/facturas_meta")
 
 print("Silver listo")
 spark.stop()
